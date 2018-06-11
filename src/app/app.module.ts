fcb2c86de37624859786572f46d09287c0fc5f8b
Velmurugan Balasubramanian <velmurugan1124@gmail.com>
2018-06-11 22:22:39 +0530

    test

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,14 @@ import { environment } from '../environments/environment';
 export const firebaseConfig = environment.firebaseConfig;
 import { AngularFireModule } from 'angularfire2';
 import { FormsModule } from '@angular/forms';
+import { AppRoutingModule } from './app-routing.module';
+
 
 import { AppComponent } from './app.component';
 import { UniversityRegistationComponent } from './university-registation/university-registation.component';
 import { UniRegistrationComponent } from './uni-registration/uni-registration.component';
 import { UniLoginComponent } from './uni-login/uni-login.component';
+import { HomeComponent } from './home/home.component';
 
 
 
@@ -19,13 +22,16 @@ import { UniLoginComponent } from './uni-login/uni-login.component';
     UniversityRegistationComponent,
     UniRegistrationComponent,
     UniLoginComponent,
+    HomeComponent,
 
   ],
   imports: [
     BrowserModule,
     CoreModule,
+    AppRoutingModule,
     FormsModule,
     AngularFireModule.initializeApp(firebaseConfig)
+
   ],
   providers: [],
   bootstrap: [AppComponent]
